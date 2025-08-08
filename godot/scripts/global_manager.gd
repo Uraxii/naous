9a631b374fbe3b37d11b07d6f74e8bbaf107c54a
Uraxii (Nicole-Brandon) <accounts@brandonpaul.me>
2025-08-08 19:46:27 -0400

    renamed game_man to game

--- godot/scripts/global_manager.gd
+++ godot/scripts/global_manager.gd
@@ -7,7 +7,7 @@ class_name GlobalManager extends Node
 @onready var packets    := PacketManager.new(signal_bus)
 @onready var input      := PlayerInput.new()
 @onready var views      := ViewManager.new()
-@onready var game_man   := GameManager.new()
+@onready var game   := GameManager.new()
 
 
 func _ready() -> void:
@@ -17,5 +17,4 @@ func _ready() -> void:
     # Load order matters here!
     add_child(input)
     add_child(views)
-    add_child(game_man)
-
+    add_child(game)
