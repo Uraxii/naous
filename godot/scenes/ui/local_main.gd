8696a50811d06f56987f26a2f011c37f296b2abe
Uraxii (Nicole-Brandon) <accounts@brandonpaul.me>
2025-08-09 01:46:47 -0400

    Updated formatting

--- godot/scenes/ui/local_main.gd
+++ godot/scenes/ui/local_main.gd
@@ -19,66 +19,66 @@ const shard_id:String = "1"
 const world_scene_dir:String = "res://scenes/world/zones/"
 
 func _ready() -> void:
-	API.delegate = MockApiClientImpl.new()
-	# Remove default views that get added by singleton for regular main.tscn flow
-	Globals.views.despawn_all()
-	
-	_populate_spawnable_scenes()
+    API.delegate = MockApiClientImpl.new()
+    # Remove default views that get added by singleton for regular main.tscn flow
+    Globals.views.despawn_all()
+    
+    _populate_spawnable_scenes()
 
 # The "Auto Spawn List" is equivalent to "add_spawnable_scene" for each scene at runtime
 # This needs to be called on both client and server to specify the allow list of scenes that can be replicated from server to client
 func _populate_spawnable_scenes() -> void:
-	var world_scenes:Array[String] = [world_scene_dir]
-	
-	while not world_scenes.is_empty():
-		var path:String = world_scenes.pop_back()
-		if path.ends_with(".tscn"):
-			# Scene file
-			if OS.is_debug_build():
-				print_debug("%s: Found scene: %s" % [name, path])
-			spawner.add_spawnable_scene(path)
-		# Directory
-		elif path.ends_with("/"):
-			for resource in ResourceLoader.list_directory(path):
-				var sub_path:String = path + resource
-				world_scenes.push_back(sub_path)
-		
+    var world_scenes:Array[String] = [world_scene_dir]
+    
+    while not world_scenes.is_empty():
+        var path:String = world_scenes.pop_back()
+        if path.ends_with(".tscn"):
+            # Scene file
+            if OS.is_debug_build():
+                print_debug("%s: Found scene: %s" % [name, path])
+            spawner.add_spawnable_scene(path)
+        # Directory
+        elif path.ends_with("/"):
+            for resource in ResourceLoader.list_directory(path):
+                var sub_path:String = path + resource
+                world_scenes.push_back(sub_path)
+        
 func _on_host_pressed() -> void:
-	entry_point.hide()
-	host_level_select.show()
+    entry_point.hide()
+    host_level_select.show()
 
 func _on_join_pressed() -> void:
-	# TODO: Calling private function to skip hub connection calls
-	Globals.game_man._create_shard_connection("localhost", port, shard_id)
-	# TODO: Respond to an rpc to switch to the server's level
-	entry_point.hide()
-	
+    # TODO: Calling private function to skip hub connection calls
+    Globals.game_man._create_shard_connection("localhost", port, shard_id)
+    # TODO: Respond to an rpc to switch to the server's level
+    entry_point.hide()
+    
 func _on_start_game_pressed() -> void:
-	var level:PackedScene = load(txt_scene_path.text)
-	if not level or not level.can_instantiate():
-		push_error("Invalid level scene: %s" % txt_scene_path.text)
-		return
-	
-	# "Allow List" for which scenes added to the spawn path root are allowed to replicate
-	# Doesn't work when only called on server
-	#spawner.add_spawnable_scene(level.resource_path)
-	#await get_tree().process_frame
-	
-	var shard_config:Dictionary = {
-		"shard_id": shard_id,
-		"shard_type": "hub", 
-		"port": port,
-		"max_players": int(txt_max_players.text)
-		#"manager_host": args.get("manager-host", "localhost"),
-		#"manager_port": int(args.get("manager-port", "8081"))
-	}
-		
-	# Now start the server manager
-	var server_manager := ServerManager.new()
-	get_tree().root.add_child(server_manager)
-	server_manager.initialize_shard(shard_config)
-	
-	# TODO: Temporary test logic - need to determine where game scenes should go
-	level_container.call_deferred("add_child",level.instantiate())
-	
-	ui_root.hide()	
+    var level:PackedScene = load(txt_scene_path.text)
+    if not level or not level.can_instantiate():
+        push_error("Invalid level scene: %s" % txt_scene_path.text)
+        return
+    
+    # "Allow List" for which scenes added to the spawn path root are allowed to replicate
+    # Doesn't work when only called on server
+    #spawner.add_spawnable_scene(level.resource_path)
+    #await get_tree().process_frame
+    
+    var shard_config:Dictionary = {
+        "shard_id": shard_id,
+        "shard_type": "hub", 
+        "port": port,
+        "max_players": int(txt_max_players.text)
+        #"manager_host": args.get("manager-host", "localhost"),
+        #"manager_port": int(args.get("manager-port", "8081"))
+    }
+        
+    # Now start the server manager
+    var server_manager := ServerManager.new()
+    get_tree().root.add_child(server_manager)
+    server_manager.initialize_shard(shard_config)
+    
+    # TODO: Temporary test logic - need to determine where game scenes should go
+    level_container.call_deferred("add_child",level.instantiate())
+    
+    ui_root.hide()	
