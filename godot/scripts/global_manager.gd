e4cf5a4b1f90f8f9c1a057db172c477e1203708a
Uraxii (Nicole-Brandon) <accounts@brandonpaul.me>
2025-08-11 17:34:30 -0400

    new global from PackedScenes, changed function name.

--- godot/scripts/global_manager.gd
+++ godot/scripts/global_manager.gd
@@ -4,22 +4,24 @@ class_name GlobalManager extends Node
 @onready var launch_args := ArgParser.parse()
 @onready var log := Log.new(signal_bus)
 @onready var packets := PacketManager.new(signal_bus)
-@onready var signal_bus: SignalBus = create_global("Signals", SignalBus)
-@onready var input: InputManager = create_global("Input", InputManager)
-@onready var views: ViewManager = create_global("Views", ViewManager)
-@onready var game: GameManager = create_global("Game", GameManager)
-@onready var entities: EntityManager = create_global("Entities", EntityManager)
-@onready var camera: CameraManager = preload(
-    "res://scenes/camera_manager.tscn").instantiate()
+@onready var signal_bus: SignalBus = new_global("Signals", SignalBus)
+@onready var input: InputManager = new_global("Input", InputManager)
+@onready var views: ViewManager = new_global("Views", ViewManager)
+@onready var game: GameManager = new_global("Game", GameManager)
+@onready var entities: EntityManager = new_global("Entities", EntityManager)
+@onready var camera: CameraManager = new_global_scene(
+    "Camera", preload("res://scenes/camera_manager.tscn"))
 
 
-func create_global(node_name: String, type: GDScript) -> Node:
+func new_global(node_name: String, type: GDScript) -> Node:
     var new_global = type.new()
     new_global.name = node_name
     add_child(new_global)
     return new_global
 
 
-func _ready() -> void:
-    camera.name = "Camera"
-    add_child(camera)
+func new_global_scene(node_name: String, scene: PackedScene) -> Node:
+    var new_node = scene.instantiate()
+    new_node.name = node_name
+    add_child(new_node)
+    return new_node
