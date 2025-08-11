22308069786b4d9e900ad951fbf25ca4ca2ac27c
Uraxii (Nicole-Brandon) <accounts@brandonpaul.me>
2025-08-11 17:21:33 -0400

    Created function to initialize globals

--- godot/scripts/global_manager.gd
+++ godot/scripts/global_manager.gd
@@ -1,30 +1,25 @@
 class_name GlobalManager extends Node
 
+# Load order matters!!!
 @onready var launch_args := ArgParser.parse()
-
-@onready var signal_bus := SignalBus.new()
-@onready var log        := Log.new(signal_bus)
-@onready var packets    := PacketManager.new(signal_bus)
-@onready var input      := InputManager.new()
-@onready var views      := ViewManager.new()
-@onready var game       := GameManager.new()
-@onready var entities   := EntityManager.new()
+@onready var log := Log.new(signal_bus)
+@onready var packets := PacketManager.new(signal_bus)
+@onready var signal_bus: SignalBus = create_global("Signals", SignalBus)
+@onready var input: InputManager = create_global("Input", InputManager)
+@onready var views: ViewManager = create_global("Views", ViewManager)
+@onready var game: GameManager = create_global("Game", GameManager)
+@onready var entities: EntityManager = create_global("Entities", EntityManager)
 @onready var camera: CameraManager = preload(
     "res://scenes/camera_manager.tscn").instantiate()
 
 
-func _ready() -> void:
-    if launch_args.has("no-globals"):
-        return
+func create_global(node_name: String, type: GDScript) -> Node:
+    var new_global = type.new()
+    new_global.name = node_name
+    add_child(new_global)
+    return new_global
 
-    # Load order matters here!
-    input.name = "Input"
-    add_child(input)
-    views.name = "View"
-    add_child(views)
-    game.name = "Game"
-    add_child(game)
-    entities.name = "Entities"
-    add_child(entities)
+
+func _ready() -> void:
     camera.name = "Camera"
     add_child(camera)
