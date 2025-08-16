18220f887a4255a7d45a3208dbab6e2b096d0f03
Uraxii (Nicole-Brandon) <accounts@brandonpaul.me>
2025-08-15 22:57:42 -0400

    Adds basic spell casting functionality
    Introduces a spell component system that allows entities to cast spells.
    
    Renames `Cast.gd` to `ComponentSpell.gd` to better reflect its role as a
    component of the entity. The component is responsible for managing the
    entity's spells.
    
    Adds a hotkey bar UI element for casting spells.
    
    Adds a `DamageCaster` spell trait, which allows spells to damage the caster.
    
    Initializes entity stats (Health, Energy) with starting values.

--- godot/scripts/entities/entity.gd
+++ godot/scripts/entities/entity.gd
@@ -1,13 +1,15 @@
 class_name Entity extends Node3D
 
 #region Variables
+signal change_control(is_local: bool)
+
 const INVALID_ID: int = -1
 
 # @export var data := EntityData.new()
 @export var body: CharacterBody3D
 @export_category("Runtime Values")
 @export var id: int = INVALID_ID
-@export var local_has_control := false
+@export var local_control := false : set = _set_local_control
 
 @onready var entities := Globals.entities
 @onready var stats: Dictionary[String, Stat] = get_all_stats()
@@ -33,6 +35,11 @@ func get_all_stats() -> Dictionary[String, Stat]:
 
     return map
 #endregion
+
+func _set_local_control(value: bool) -> void:
+    local_control = value
+    change_control.emit(local_control)
+        
 
 #region Godot Callback Functions
 func _ready() -> void:
--- godot/scripts/spells/traits/damage_caster.gd
+++ godot/scripts/spells/traits/damage_caster.gd
@@ -2,6 +2,13 @@ class_name DamageCaster extends Node
 
 @export var damage := 10.0
 
+@onready var spell: Spell
+
+func setup() -> void:
+    spell = get_parent()
+
 
 func cast() -> void:
-    pass
+    var target = spell.caster
+    var health = target.get_stat("Health")
+    health.current -= damage
