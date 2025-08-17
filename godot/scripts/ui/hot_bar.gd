13e74a16d3ca067753b3d7d91f04891df5d4feed
Uraxii <nicole@nicolepaul.net>
2025-08-17 19:35:30 -0400

    Enables hotbar to update on entity control
    Connects to the control_entity signal to update the hotbar when the player controls a new entity.
    The hotbar populates with the spells of the controlled entity.

--- godot/scripts/ui/hot_bar.gd
+++ godot/scripts/ui/hot_bar.gd
@@ -6,8 +6,20 @@ var buttons: Array[HotkeyButton] = []
 
 
 func _ready() -> void:
+    signals.control_entity.connect(_on_control)
+    
     for i in range(0, binds.size()):
         var button = HotkeyButton.new()
         button.set_bind(signals.get(binds[i]))
         buttons.append(button)
         add_child(button)
+
+
+func _on_control(new_enity: Entity) -> void:
+    if not new_enity.spells:
+        return
+    
+    var spells: Array[Spell] = new_enity.spells.spells.values()
+    
+    for i in range(spells.size()):
+        buttons[i].set_spell(spells[i])
