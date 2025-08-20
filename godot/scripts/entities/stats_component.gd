61b5699503512ae594a35a76cf60015069833809
Link of Origin <jesselink333@gmail.com>
2025-08-20 16:07:27 -0600

    Update references of 'Stat' to new 'StatComponent' name

--- godot/scripts/entities/stats_component.gd
+++ godot/scripts/entities/stats_component.gd
@@ -1,21 +1,21 @@
 class_name StatsComponent extends Node
 
-@onready var stats: Dictionary[String, Stat] = get_all_stats()
+@onready var stats: Dictionary[String, StatComponent] = get_all_stats()
 
 
-func get_stat(stat_id: String) -> Stat:
+func get_stat(stat_id: String) -> StatComponent:
     if not stats.has(stat_id):
         stats = get_all_stats()
 
     return stats.get(stat_id)
 
 
-func get_all_stats() -> Dictionary[String, Stat]:
-    # Get any child that is of type Stat
-    var stat_nodes := get_children().filter(func(child): return child is Stat)
+func get_all_stats() -> Dictionary[String, StatComponent]:
+    # Get any child that is of type StatComponent
+    var stat_nodes := get_children().filter(func(child): return child is StatComponent)
 
-    var map: Dictionary[String, Stat] = {}
-    for s: Stat in stat_nodes:
+    var map: Dictionary[String, StatComponent] = {}
+    for s: StatComponent in stat_nodes:
         map[s.name] = s
 
     return map
