2d3d3d5c06794d27960aab398b780763cacdcccf
Uraxii (Nicole-Brandon) <accounts@brandonpaul.me>
2025-08-20 16:16:16 -0400

    Adds entity replication
    Adds an entity synchronizer to handle networked state replication.
    
    This includes replicating health, energy, speed, gravity and jump force.
    Also adds a RemoteTransform to keep body transforms synchronised.

--- godot/scripts/entities/entity_synchronizer.gd
+++ godot/scripts/entities/entity_synchronizer.gd
@@ -1,8 +1,1 @@
 class_name EntitySynchronizer extends MultiplayerSynchronizer
-
-
-func sync_stats() -> void:
-    var entity: Entity = get_parent()
-    if not entity:
-        push_warning("EntitySynchronizer could not find entit.")
-        return
