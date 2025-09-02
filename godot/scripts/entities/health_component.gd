bc97fdfeed90711ad608b484139cda7276edd5b5
Uraxii <nicole@nicolepaul.net>
2025-09-02 00:58:20 -0400

    Health component calls die when value is 0
    The health component connects to an entity and listens
    for value changes. If the health value drops to zero or
    below, it triggers the entity's death function. It also
    includes an error check to ensure the entity is assigned.

--- godot/scripts/entities/health_component.gd
+++ godot/scripts/entities/health_component.gd
@@ -1,1 +1,15 @@
 class_name HealthComponent extends StatComponent
+
+@export var entity: Entity
+
+
+func _on_value_change(new: float, _old: float) -> void:
+    if new <= 0:
+        entity.die()
+
+
+func _ready() -> void:
+    if not entity:
+        push_error("HealthComponent is missing Entity assignment!")
+
+    change.connect(_on_value_change)
