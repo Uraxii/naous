4d2854e3417ef329ae1dce4196f688cf5fdf4503
Uraxii <nicole@nicolepaul.net>
2025-09-02 00:58:31 -0400

    Initializes stat current value on ready
    Sets the current stat value to the start value on ready,
    ensuring a consistent initial state.
    
    Removes redundant current setting in _ready function.

--- godot/scripts/entities/stat.gd
+++ godot/scripts/entities/stat.gd
@@ -7,20 +7,20 @@ signal change(new: float, old: float)
 @export var start_value := 1.0  
 @export var min_value := 0.0 
 @export var max_value := 10.0
-
 @export_category("Runtime Values")
 @export var is_infinite := false
-@export var current: float: set = _set_current
+
+@onready var current: float = start_value: set = _set_current
+
+var signals: SignalBus:
+    get: return Globals.signal_bus
 #endregion
-
-
-func _ready() -> void:
-    current = start_value
 
 
 func _set_current(value: float) -> void:
     if is_infinite:
         return
+        
     var old = current
     current = clamp(value, min_value, max_value)
     change.emit(current, old)
