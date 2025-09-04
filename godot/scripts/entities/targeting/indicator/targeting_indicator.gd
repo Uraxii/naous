fb2af79aebd8015f51b7adfa6c3d2993aae5c3d5
Link of Origin <jesselink333@gmail.com>
2025-09-04 17:25:01 -0600

    More indicator progress
    Circle is kinda working, pointer is kinda working. Need to better account for node scaling. Thinking the logic needs to move the the entity object

--- godot/scripts/entities/targeting/indicator/targeting_indicator.gd
+++ godot/scripts/entities/targeting/indicator/targeting_indicator.gd
@@ -1,25 +1,28 @@
 class_name TargetingIndicator extends Node3D
 
+@export var entity: Entity:
+    set = set_owning_entity
+
+const POINTER_BUFFER := 0.5
 const RADIUS_BUFFER := 0.3
 
 @onready var circle: MeshInstance3D = %Circle
 @onready var pointer: MeshInstance3D = %Pointer
 
-var entity: Entity:
-    set = set_owning_entity
-
 
 func set_owning_entity(owning_entity: Entity) -> void:
-    entity = owning_entity
-    adjust_ground_indicator_for_entity(entity)
-    adjust_pointer_indicator_for_entity(entity)
+    if is_inside_tree():
+        entity = owning_entity
+        adjust_ground_indicator_for_entity(entity)
+        adjust_pointer_indicator_for_entity(entity)
 
 
 func adjust_ground_indicator_for_entity(target_entity: Entity) -> void:
-    var target_visual_instances := target_entity.find_children("*", "VisualInstance3D")
-    var first_visual_instance: VisualInstance3D = target_visual_instances[0] as VisualInstance3D
-    var target_aabb := first_visual_instance.get_aabb()
-    var transformed_aabb := target_aabb * first_visual_instance.global_transform
+    var transformed_aabb := _get_transformed_target_aabb(target_entity)
+    var new_circle_height := transformed_aabb.position.y
+    circle.global_position.y = new_circle_height
+    print("updated ground ind height for mesh %s = %s" % [entity.name, circle.global_position.y])
+    
     var circle_radius := sqrt(pow(transformed_aabb.size.x, 2) + pow(transformed_aabb.size.z, 2)) / 2
     var torus_mesh: TorusMesh = circle.mesh as TorusMesh
     var concentric_radius_diff := torus_mesh.outer_radius - torus_mesh.inner_radius
@@ -29,14 +32,26 @@ func adjust_ground_indicator_for_entity(target_entity: Entity) -> void:
 
 
 func adjust_pointer_indicator_for_entity(target_entity: Entity) -> void:
-    pass
-
+    var transformed_aabb := _get_transformed_target_aabb(target_entity)
+    var new_pointer_height := transformed_aabb.end.y + POINTER_BUFFER
+    pointer.global_position.y = new_pointer_height
+    print("updated pointer height for mesh %s = %s" % [entity.name, pointer.global_position.y])
+    
 
 func set_material_color(new_color: Color) -> void:
     var circle_mesh := circle.mesh.surface_get_material(0)
     circle_mesh.color = new_color
     var pointer_mesh := pointer.mesh.surface_get_material(0)
     pointer_mesh.color = new_color
+
+
+func _get_transformed_target_aabb(target_entity: Entity) -> AABB:
+    # FIXME: Account for scaling of nodes (need to pass this to Entity)
+    var target_visual_instances := target_entity.find_children("*", "VisualInstance3D")
+    var first_visual_instance: VisualInstance3D = target_visual_instances[0] as VisualInstance3D
+    var target_aabb := first_visual_instance.get_aabb()
+    var transformed_aabb := target_aabb * first_visual_instance.global_transform.affine_inverse()
+    return transformed_aabb
 
 
 #region Godot Callbacks
