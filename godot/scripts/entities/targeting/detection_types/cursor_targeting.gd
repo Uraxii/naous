897d60e66d495e7bcd7ffc96bcc94f21bc23cf35
Link of Origin <jesselink333@gmail.com>
2025-09-07 12:11:42 -0600

    Updated Cursor targeting to also utilize a raycast
    Previously it just used a shape detector looking for AABB vertices. Now will use a single raycast at the cursor location to find objects as well. Better for the more intuitive action of attempting to "click" on or select the center of the targetable body.

--- godot/scripts/entities/targeting/detection_types/cursor_targeting.gd
+++ godot/scripts/entities/targeting/detection_types/cursor_targeting.gd
@@ -1,7 +1,15 @@
 class_name CursorTargeting extends TargetingDetection
 
+@export_flags_3d_physics var raycast_collision_mask: int
+
+const RAYCAST_LENGTH := 500.0
+
+@onready var camera_manager := Globals.camera
 @onready var camera := Globals.camera.camera
+
 var cursor_detector: TargetDetector
+
+var _raycast_targets: Array[Targetable]
 
 
 func sync_detectors_to_cursor(cursor_pos: Vector2) -> void:
@@ -20,8 +28,74 @@ func _target_sort(target_A: Targetable, target_B: Targetable) -> bool:
     return target_A_distance_to_cursor < target_B_distance_to_cursor
 
 
+func update_raycast_targets() -> void:
+    _raycast_targets.clear()
+    
+    # TODO: Currently just detecting 1 target per raycast. If we expand this to look for multiple (ie. the raycast "pierces" the first object to find more), we just need to update this to handle a list of objects instead of just one.
+    var mouse_position := get_viewport().get_mouse_position()
+    var current_targetable := find_targetable_at_cursor_position(mouse_position)
+    if is_instance_valid(current_targetable):
+        # TODO: Add prioritization logic to list of raycast targets
+        _raycast_targets.push_back(current_targetable)
+        update_prioritized_targets_for_detectors()
+
+
+func find_targetable_at_cursor_position(cursor_pos: Vector2) -> Targetable:
+    var player_body: CharacterBody3D = camera_manager.target
+    if not is_instance_valid(player_body):
+        return null
+    
+    var found_targetable: Targetable = null
+    
+    # Prep our raycast details
+    var world_space_3d := camera.get_world_3d().direct_space_state
+    var ray_from := camera.project_ray_origin(cursor_pos)
+    var ray_to := camera.project_ray_normal(cursor_pos) * RAYCAST_LENGTH
+    var ray_collision_mask := raycast_collision_mask
+    # TODO: Unused unless we start looking for multiple objects in the future. We'd loop around some condition and keep casting while adding each found object to this list to prevent it from beign detected again.
+    var excluded_objects: Array[RID]
+    excluded_objects.push_back(player_body.get_rid())
+    var ray_query_params := PhysicsRayQueryParameters3D.create(ray_from, ray_to, ray_collision_mask, excluded_objects)
+    
+    # Cast the ray and check for collisions
+    var intersection_result := world_space_3d.intersect_ray(ray_query_params)
+    if not intersection_result.is_empty():
+        var entity_body_found: CharacterBody3D = intersection_result.get("collider")
+        # If the cast failed, we'll get a null object
+        if entity_body_found != null:
+            var targetable_entity: Targetable
+            # Find a valid Targetable Entity that contains the body we found
+            var valid_targets := targeting.get_valid_targets()
+            for valid_target: Targetable in valid_targets:
+                var target_entity := valid_target.entity
+                if target_entity.body == entity_body_found:
+                    targetable_entity = target_entity.components.find("Targetable")
+                    if targetable_entity != null:
+                        #print("Cursor raycast found Target: ", targetable_entity.get_parent().get_parent().name)
+                        found_targetable = targetable_entity
+    
+    return found_targetable
+
+
+func _get_currently_detected_targets() -> Array[Targetable]:
+    var detector_targets := super._get_currently_detected_targets()
+    
+    for raycast_target: Targetable in _raycast_targets:
+        # Remove the object from the detectors list if the raycast already found it
+        if detector_targets.has(raycast_target):
+            detector_targets.erase(raycast_target)
+        
+        detector_targets.push_front(raycast_target)
+    
+    return detector_targets
+    
+
 #region Godot Callbacks
 func _process(_delta: float) -> void:
     var cursor_position: Vector2 = get_viewport().get_mouse_position()
     sync_detectors_to_cursor(cursor_position)
+
+
+func _physics_process(_delta: float) -> void:
+    update_raycast_targets()
 #endregion
--- godot/scripts/entities/targeting/detection_types/targeting_detection.gd
+++ godot/scripts/entities/targeting/detection_types/targeting_detection.gd
@@ -1,9 +1,12 @@
 class_name TargetingDetection extends Node
 
-## List of TargetDetector nodes to use for the detection logic. Order matters! First has highest priority, last has lowest priority.
+## List of TargetDetector nodes to use for the detection logic. Order matters! First has highest priority, last has lowest priority. If this is left unset, the child TargetDetector nodes of this scene will be used to populate this value with nodes near the "top" of the tree given higher priority.
 @export var detectors: Array[TargetDetector]
+## When enabled, displays the detector shapes visually on the screen.
 @export var enable_debug_view: bool = false:
     set = set_debug_mode_for_detectors
+
+@onready var targeting := Globals.targeting
 
 var prioritized_target_list: Array[Targetable]
 
@@ -74,16 +77,24 @@ func get_next_target_by_higher_priority(target: Targetable) -> Targetable:
 func update_prioritized_targets_for_detectors() -> void:
     #print("updating priority targets! - ", self.name)
     prioritized_target_list.clear()
+    var currently_detected_targets := _get_currently_detected_targets()
+    prioritized_target_list.assign(currently_detected_targets)
+    _queued_for_target_updated = false
+
+
+## Returns a prioritized list of currently detected targets. Can be overridden to extend the logic beyond just the Detectors
+func _get_currently_detected_targets() -> Array[Targetable]:
+    var detected_targets: Array[Targetable]
     for detector: TargetDetector in detectors:
         var detector_targets := detector.get_current_targets()
         if detector_targets.size() > 1:
             detector_targets = _prioritize_detector_targets(detector_targets)
         for target: Targetable in detector_targets:
-            if not prioritized_target_list.has(target):
+            if not detected_targets.has(target):
                 #print("Frame %s: '%s' found target '%s' via detector '%s'" % [Engine.get_frames_drawn(), self.name, target.get_parent().get_parent().name, detector.name])
-                prioritized_target_list.push_back(target)
-    _queued_for_target_updated = false
-
+                detected_targets.push_back(target)
+    return detected_targets
+    
 
 ## Sorts a list of targets found by a single Detector (Detector order itself determines default priority order)
 func _prioritize_detector_targets(targets: Array[Targetable]) -> Array[Targetable]:
@@ -92,13 +103,28 @@ func _prioritize_detector_targets(targets: Array[Targetable]) -> Array[Targetabl
     return prioritized_targets
 
 
-func _on_detector_targets_changed(_targets: Array[Targetable]) -> void:
+func _attempt_queue_for_target_updates() -> void:
     if not _queued_for_target_updated:
         # We only want to execute this once per frame if multiple detectors signal on the same frame
         # This felt cleaner than testing every frame via '_process'
         _queued_for_target_updated = true
         update_prioritized_targets_for_detectors.call_deferred()
+    
 
+func _on_detector_targets_changed(_targets: Array[Targetable]) -> void:
+    _attempt_queue_for_target_updates()
+
+
+func _set_detectors_from_children() -> void:
+    detectors.clear()
+    
+    var detector_children := find_children("*", "TargetDetector")
+    if not detector_children.is_empty():
+        for detector: TargetDetector in detector_children:
+            # I can't find documentation stating that 'find_children' returns a determistic order of child nodes (such as in scene-tree order as see in the editor). Using the node's index to be 100% sure that the order is maintained.
+            var detector_child_index := detector.get_index()
+            detectors.insert(detector_child_index, detector)
+        
 
 #region Debugging
 func set_debug_mode_for_detectors(enable_debug: bool) -> void:
@@ -119,6 +145,10 @@ func _target_sort(target_A: Targetable, target_B: Targetable) -> bool:
 
 #region Godot Callbacks
 func _ready() -> void:
+    # Fallback in case the user didn't set these manually
+    if detectors.is_empty():
+        _set_detectors_from_children()
+    
     set_debug_mode_for_detectors(enable_debug_view)
     for detector: TargetDetector in detectors:
         detector.current_targets_updated.connect(_on_detector_targets_changed)
