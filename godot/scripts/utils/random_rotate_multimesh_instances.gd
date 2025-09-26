918b10c92ae56df0ec2d69f2e2119059225062cd
Christer Kaitila <breakdance@mcfunkypants.com>
2025-09-26 14:06:33 -0700

    excessive amounts of grass added to lake natalie (150k)

--- godot/scripts/utils/random_rotate_multimesh_instances.gd
+++ godot/scripts/utils/random_rotate_multimesh_instances.gd
@@ -6,9 +6,6 @@ extends MultiMeshInstance3D
 func apply_random_y_rotation():
     for i in range(0,  multimesh.instance_count):
         var current_trans = multimesh.get_instance_transform(i)
-        # damn this seems to not work right........... arghhhh so close
-        # current_trans.basis = Basis.IDENTITY
-        # current_trans = current_trans.rotated(Vector3.UP, randf_range(-PI, PI))
         current_trans = current_trans.rotated_local(Vector3.UP, randf_range(-PI, PI))
         multimesh.set_instance_transform(i, current_trans)
 
