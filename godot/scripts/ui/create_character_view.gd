becd429a2c621d6b360a3b49c733656c9f32fe0c
Christer Kaitila <breakdance@mcfunkypants.com>
2025-10-30 10:20:07 -0700

    remove unused variable warning and fix typo

--- godot/scripts/ui/create_character_view.gd
+++ godot/scripts/ui/create_character_view.gd
@@ -4,6 +4,8 @@ class_name CreateCharacterView extends View
 func _ready() -> void:
     print("hello")
     _connect_signals()
+    # start the animation just in case it isn't set to do so by default
+    $AnimationPlayer.play("idle") # assumes this animation exists
 
 
 func _connect_signals() -> void:
@@ -21,12 +23,12 @@ func _on_back_pressed() -> void:
 
     
 func _on_create_pressed() -> void:
-    var character_name: LineEdit = %CharacterName
-    ##API.create_character(character_name.text)
+    var _character_name: LineEdit = %CharacterName
+    ##API.create_character(_character_name.text)
 
 
 func _on_character_created(data: Dictionary) -> void:
-    print("Characte created: ", data)
+    print("Character created: ", data)
     _return_to_character_select()
 
 
