ced361191e9c11d13cb375d61b94916546f4d231
Uraxii <accounts@nicolepaul.net>
2025-11-24 10:42:04 -0500

    Formatting

--- godot/scripts/entity_manager.gd
+++ godot/scripts/entity_manager.gd
@@ -1,7 +1,7 @@
 class_name EntityManager extends MultiplayerSpawner
 
 @export var player_scene: PackedScene = preload(
-	"uid://c5ospakgw0cwt")
+    "uid://c5ospakgw0cwt")
 
 @onready var signals: SignalBus = Globals.signal_bus
 @onready var lg: Log = Globals.logger
@@ -12,56 +12,56 @@ var _ids := IdPool.new()
 
 @rpc("call_local")
 func despawn(id: int) -> void:
-	if not multiplayer.is_server():
-		return
+    if not multiplayer.is_server():
+        return
 
-	var entity: Entity = pool.get(id)
-	pool.erase(id)
+    var entity: Entity = pool.get(id)
+    pool.erase(id)
 
-	if not entity or not entity.is_inside_tree():
-		return
+    if not entity or not entity.is_inside_tree():
+        return
 
-	entity.queue_free.call_deferred()
-	#lg.debug(entity)
+    entity.queue_free.call_deferred()
+    #lg.debug(entity)
 
 
 func find(id: int) -> Entity:
-	return pool.get(id)
+    return pool.get(id)
 
 
 func _on_client_spawn(node: Node) -> void:
-	if node is Entity:
-		# Register the entity in the pool.
-		pool[node.id] = node as Entity
-		#lg.debug("Registered %s as %d" % [node.name, node.id])
+    if node is Entity:
+        # Register the entity in the pool.
+        pool[node.id] = node as Entity
+        #lg.debug("Registered %s as %d" % [node.name, node.id])
 
 
 #region Godot Callback Functions
 func _ready():
-	spawn_function = Callable(self, "_spawn_custom")
-	spawned.connect(_on_client_spawn)
+    spawn_function = Callable(self, "_spawn_custom")
+    spawned.connect(_on_client_spawn)
 
 
 func _spawn_custom(data: Dictionary) -> Node:
-	# TODO: CHANGE THIS!!! PLAYERS SHOULD NOT BE ABLE TO PASS IN AN ABITRARY PATH!!!
-	var scene = load(data.scene)
-	var entity: Entity = scene.instantiate()
-	
-	if data.get("type") == "player":
-		entity.transform_sync.set_multiplayer_authority(data.authority)
-		entity.name = data.id
-		
-	if data.has("position"):
-		print_debug("Set entity pos to %s" % data.position)
-		entity.position = data.position
+    # TODO: CHANGE THIS!!! PLAYERS SHOULD NOT BE ABLE TO PASS IN AN ABITRARY PATH!!!
+    var scene = load(data.scene)
+    var entity: Entity = scene.instantiate()
+    
+    if data.get("type") == "player":
+        entity.transform_sync.set_multiplayer_authority(data.authority)
+        entity.name = data.id
+        
+    if data.has("position"):
+        print_debug("Set entity pos to %s" % data.position)
+        entity.position = data.position
 
-	if "active_character" in data:
-		entity.active_character = data["active_character"]
-		
-	var id = _ids.lease()
-	entity.id = id
-	pool[id] = entity
+    if "active_character" in data:
+        entity.active_character = data["active_character"]
+        
+    var id = _ids.lease()
+    entity.id = id
+    pool[id] = entity
 
-	signals.spawn_entity.emit(entity)
-	return entity
+    signals.spawn_entity.emit(entity)
+    return entity
 #endregion
