28f0c139738d832c093c2929af1bd588b2e375c1
Uraxii <accounts@nicolepaul.net>
2025-11-30 16:50:42 -0500

    fixing issue with instance api files not syncing

---
+++ godot/scripts/instance_stuff/instance_config.gd
@@ -0,0 +1,10 @@
+class_name InstanceConfig extends Resource
+
+@export var host    := "localhost"
+@export var port    := 9000
+@export var size    := 99
+@export var disconnect_delay_sec := 300
+@export var level := preload("res://scenes/world/zones/lake-natalie.tscn")
+@export var player_scene := preload("res://scenes/entities/player.tscn")
+
+#comment to trigger git
