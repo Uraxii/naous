efa88e3fa9f3920ab944ff5a2bcc8de3f5b3cdfa
Uraxii <accounts@nicolepaul.net>
2025-12-08 19:50:13 -0500

    Formatting

--- godot/scripts/spells/projectile.gd
+++ godot/scripts/spells/projectile.gd
@@ -12,20 +12,20 @@ func calculate_direction():
 func _physics_process(delta) -> void:
     if not multiplayer.is_server():
         return
-        
+
     var collision = body.move_and_collide(velocity * delta)
     if collision:
         _hit_target(collision)
-        
+
 
 func _hit_target(collision: KinematicCollision3D) -> void:    
     var hit_object = collision.get_collider()
-    
+
     var hit_entity: Entity = hit_object.get("entity")
     if not hit_entity:
         return
-        
+
     if hit_entity.health:
         hit_entity.health.current -= damage
-    
+
     queue_free()
