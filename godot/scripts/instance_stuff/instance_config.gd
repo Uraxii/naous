0b6e2e3ebcec0472ebf95515ef62124b240d8de8
Uraxii <accounts@nicolepaul.net>
2025-12-08 19:53:45 -0500

    added tick rate to config

--- godot/scripts/instance_stuff/instance_config.gd
+++ godot/scripts/instance_stuff/instance_config.gd
@@ -6,5 +6,7 @@ class_name InstanceConfig extends Resource
 @export var disconnect_delay_sec := 300
 @export var level := preload("res://scenes/world/zones/lake-natalie.tscn")
 @export var player_scene := preload("res://scenes/entities/player.tscn")
+@export var tick_rate := 30
 
-#comment to trigger git
+var tick_interal: float:
+    get: return 1.0 / tick_rate
