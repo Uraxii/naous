c732a7df4ae56840f79407571388688ea0a55303
Uraxii <accounts@nicolepaul.net>
2025-12-12 23:13:52 -0500

    Fixed stat view bug

--- godot/scripts/ui/stat_view.gd
+++ godot/scripts/ui/stat_view.gd
@@ -1,6 +1,6 @@
 class_name StatView extends View
 
-@export var stat_type := "Health"
+@export var stat_id := ComponentStat.HEALTH_ID
 @export var label: Label
 
 var stat: ComponentStat
@@ -9,11 +9,11 @@ var stat: ComponentStat
 func set_entity(new_entity: Entity) -> void:
     if stat and stat.change.is_connected(_on_change):
         stat.change.disconnect(_on_change)
-        
-    stat = new_entity.components.find(stat_type)
+
+    stat = new_entity.stats.find(stat_id)
     stat.change.connect(_on_change)
     _on_change(stat.current, stat.current)
-    
+
 
 func _on_change(new: float, _old: float) -> void:
     label.text = str(new)
