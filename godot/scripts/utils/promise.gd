9eda2675f9cee435ca6d87320f495f645596b2ac
Uraxii <accounts@nicolepaul.net>
2025-12-23 12:58:15 -0500

    Promise IDs now have default value of -1

--- godot/scripts/utils/promise.gd
+++ godot/scripts/utils/promise.gd
@@ -2,9 +2,13 @@ class_name Promise
 
 signal trigger(data)
 
-var id: int
+## Value for invalid ID.
+const BAD_ID := -1
+
+## Default is bad value.
+var id := BAD_ID
 
 
-func _init(promise_id: int) -> void:
+func _init(promise_id := id) -> void:
     self.id = promise_id
 
