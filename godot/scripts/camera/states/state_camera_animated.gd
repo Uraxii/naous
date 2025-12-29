bb3b15fe826df406497b8e0045097494e5ea9ff1
tarnished moth <87274781+tarnishedmoth@users.noreply.github.com>
2025-12-29 17:31:32 -0500

    Parsing a text file for headers and body into UI Label elements

---
+++ godot/scripts/camera/states/state_camera_animated.gd
@@ -0,0 +1,67 @@
+class_name StateCameraAnimated extends State
+
+@onready var camera: CameraManager = get_owner()
+
+var animation_player: AnimationPlayer:
+    get: return # TODO
+
+var input: InputManager:
+    get: return Globals.input
+
+
+func enter() -> void:
+    next_state = StateCameraAnimated
+
+
+func process() -> void:
+    if not animation_player or not animation_player.is_inside_tree():
+        next_state = StateCameraFree
+        return
+
+    ## TODO set camera.position to match a Marker animated by the player?
+
+    ## Handle mouse motion -- probably do nothing
+    #if not camera.direction.is_zero_approx():
+        #_handle_rotation(camera.direction * camera.mouse_sensitivity)
+        #camera.direction = Vector2.ZERO
+
+    ## Handle controller joystick
+    ## - Can't drive this via signals since the joystick doesn't constantly emit events while
+    ##   actuated outside of neutral. So we just check the input directly. Not elegant, but works.
+    #elif not input.joystick_camera.is_zero_approx():
+        #var joystick_look := input.joystick_camera
+        #if camera.invert_look_y_axis:
+            #joystick_look *= Vector2(1, -1)
+#
+        #_handle_rotation(joystick_look * camera.joystick_sensitivity)
+
+    ## Handle menu
+    elif camera.menu_is_open:
+        if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+            Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+
+func exit() -> void:
+    pass
+
+## DEPRECATED
+func get_next_position(position: Vector3) -> Vector3:
+    var new_position = Vector3(
+        position.x + camera.x_offset,
+        position.y + camera.y_offset,
+        position.z + camera.z_offset
+        )
+
+    return new_position
+
+
+## DEPRECATED
+func _handle_rotation(rotation_vector: Vector2) -> void:
+    camera.rotation.y -= rotation_vector.x
+    camera.rotation.x -= rotation_vector.y
+
+    if camera.rotation.x < -1:
+        camera.rotation.x = -1
+    
+    #if camera.rotate_entity:
+        #target.rotation.y = camera.rotation.y
---
+++ godot/scripts/ui/credits.gd
@@ -0,0 +1,52 @@
+extends Node
+
+## Populates the visible credits.
+## A simple syntax is employed to format the result. "-" indicate titles. You can optionally
+## include a description in the line following an indented line.
+
+@export_file("*.txt") var credits_path: String ## Text file. No special features or parsing yet.
+@export_multiline var credits_text: String ## Will be used if [member credits_path] is empty.
+
+@onready var template_header: Label = %TemplateHeader
+@onready var template_body: Label = %TemplateBody
+@onready var credits_items: VBoxContainer = %CreditsItems
+
+func _ready() -> void:
+	template_header.hide()
+	template_body.hide()
+	
+	var credits_data: String
+	if credits_path:
+		var file = FileAccess.open(credits_path, FileAccess.READ)
+		credits_data = file.get_as_text() if file != null else credits_text
+		file.close()
+	else:
+		credits_data = credits_text
+	populate_with(credits_data)
+		
+func populate_with(text: String) -> void:
+	## Clear all existing entries
+	#for child in credits_items.get_children():
+		#if child == template_header or child == template_body:
+			#pass
+		#else:
+			#child.queue_free()
+	
+	## Parse it
+	var splits: PackedStringArray = text.split("\n", false)
+	
+	for split: String in splits:
+		var new_item: Label
+		var new_text: String
+		if split.begins_with("-"):
+			## Title/heading
+			new_item = template_header.duplicate()
+			new_text = split.trim_prefix("-")
+		else:
+			new_item = template_body.duplicate()
+			new_text = split
+			
+		new_item.text = new_text
+		new_item.visible = true
+		
+		credits_items.add_child(new_item)
