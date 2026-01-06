834a4b43a9e57c65fecf4af5596a1fe18d771dff
tarnished moth <87274781+tarnishedmoth@users.noreply.github.com>
2026-01-06 11:56:12 -0500

    Fixed broken UID in const

--- godot/scenes/world/dungeons/coalescence/tutorial/tutorial_music.gd
+++ godot/scenes/world/dungeons/coalescence/tutorial/tutorial_music.gd
@@ -1,20 +1,24 @@
 class_name TutorialMusic
 extends Node
 
-const NOT_SO_STILL: DynamicMusicTrack = preload("uid://b4cwpyvpt3f8n")
-const TRACK_2: DynamicMusicTrack = preload("uid://brq7gfor30m8x")
-const AMBIVALENT_FELLOW: DynamicMusicTrack  = preload("uid://bay64fi84hggh")
-const COMBAT: DynamicMusicTrack = preload("uid://dbkj5mkwehrw4")
-const TRACK_5: DynamicMusicTrack = preload("uid://bcpglwarj06jc")
-const FINALE: DynamicMusicTrack = preload("uid://bijg3owlp50gm")
+const INITIAL_VOLUME: float = 0.2
 
+const NOT_SO_STILL: DynamicMusicTrack = preload("uid://e26byi4f045n")
+const INTO_OASIS: DynamicMusicTrack = preload("uid://brq7gfor30m8x")
+const AMBIVALENT_FELLOW: DynamicMusicTrack  = preload("uid://bay64fi84hggh")
+const ADVERSE_ENTITY: DynamicMusicTrack = preload("uid://dbkj5mkwehrw4")
+const COALESCENT_DISCOVERIES: DynamicMusicTrack = preload("uid://bcpglwarj06jc")
+const ECHOES_FINALE: DynamicMusicTrack = preload("uid://bijg3owlp50gm")
+
+func _ready() -> void:
+    DynamicMusicManager.set_music_bus_volume(INITIAL_VOLUME)
 
 func play_idle() -> void:
     Globals.music.start_track(NOT_SO_STILL)
 
 
 func play_combat() -> void:
-    Globals.music.start_track(COMBAT)
+    Globals.music.start_track(ADVERSE_ENTITY)
 
 
 func play_background() -> void:
@@ -22,16 +26,12 @@ func play_background() -> void:
 
 
 func play_explore() -> void:
-    Globals.music.start_track(TRACK_2)
+    Globals.music.start_track(INTO_OASIS)
 
 
 func play_boss_combat() -> void:
-    Globals.music.start_track(TRACK_5)
+    Globals.music.start_track(COALESCENT_DISCOVERIES)
     
 
 func play_finale() -> void:
-    Globals.music.start_track(FINALE)
-
-
-func _ready() -> void:
-    DynamicMusicManager.set_music_bus_volume(0.2)
+    Globals.music.start_track(ECHOES_FINALE)
